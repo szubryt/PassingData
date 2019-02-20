cf418062bec891332ba8332bba041a897d2ebc05
Piotr Szubryt <piotr@borsuquesquad.pl>
2019-02-20 09:54:12 +0100

    Custom text from textfield

--- PassingData/src/Presser.js
+++ PassingData/src/Presser.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 
 class Presser extends Component {
 
     constructor(props) {
-        super();
+        super(props);
         this.state = {
-            text: "Text has been changed !"
+            text: ""
         };
     }
 
@@ -16,11 +16,28 @@ class Presser extends Component {
 
     render() {
       return (
+          <View>
         <Button
         onPress={this.onChangeText.bind(this)}
         title="Change the TEXT"
         color="#841584"
+        style={{
+            width: 500,
+            height: 40,
+            margin: 20
+            }}
       />
+      <TextInput
+        style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            margin: 20
+            }}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+      </View>
         );
     }
 }
